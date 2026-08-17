e6cce187299f4f772cd40f846207fbdd02727791
Steven Iannucci <89707282+steveniannucci@users.noreply.github.com>
2026-08-16 21:38:01 -0400

    Added content warning filters

--- index.js
+++ index.js
@@ -13,16 +13,44 @@ if (storedTitle !== null) {
     currentTitle = storedTitle;
 }
 const storedTextColor = localStorage.getItem("text-color")
-if (storedTextColor !== null) {
+if (storedTextColor === null) {
+    localStorage.setItem("text-color", currentTextColor);
+} else {
     document.body.style.color = storedTextColor;
 }
 const storedOutlineColor = localStorage.getItem("outline-color")
-if (storedOutlineColor !== null) {
+if (storedOutlineColor === null) {
+    localStorage.setItem("outline-color", currentOutlineColor);
+} else {
     document.body.style.textShadow = `1px 1px 0 ${storedOutlineColor}`
 }
 const storedMovieNumber = localStorage.getItem("locked-movie-number");
 const storedBackdropNumber = localStorage.getItem("locked-backdrop-number");
 const storedDisplayTaglineCheckedValue = localStorage.getItem("display-tagline");
+const allowMatureRatedContent = localStorage.getItem("allow-mature-rated-content");
+const allowPG13RatedContent = localStorage.getItem("allow-pg-13-rated-content");
+const allowHorrorGenreContent = localStorage.getItem("allow-horror-genre-content");
+const allowRomanceGenreContent = localStorage.getItem("allow-romance-genre-content");
+const allowThrillerGenreContent = localStorage.getItem("allow-thriller-genre-content");
+
+if (storedDisplayTaglineCheckedValue === null) {
+    localStorage.setItem("display-tagline", true);
+}
+if (allowMatureRatedContent === null) {
+    localStorage.setItem("allow-mature-rated-content", false);
+}
+if (allowPG13RatedContent === null) {
+    localStorage.setItem("allow-pg-13-rated-content", true);
+}
+if (allowHorrorGenreContent === null) {
+    localStorage.setItem("allow-horror-genre-content", false);
+}
+if (allowRomanceGenreContent === null) {
+    localStorage.setItem("allow-romance-genre-content", true);
+}
+if (allowThrillerGenreContent === null) {
+    localStorage.setItem("allow-thriller-genre-content", true);
+}
 
 document.querySelector("#time").textContent = new Date().toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"});
 function updateTime() {
@@ -31,11 +59,12 @@ function updateTime() {
 setInterval(updateTime, 1000);
 
 const currentYear = new Date().getFullYear();
-// const quote = document.querySelector("#quote").textContent = "\"With great power comes great responsibility.\""
 
-// const quotes = ["With great power comes great responsibility.", "Be the change you want to see in the world.", "How did we get here?", "Happiness can be found even in the darkest of times, if one only remembers to turn on the light.", "Did you... ever do something that you regret?"]
-// const randomQuote = Math.floor(Math.random() * quotes.length);
-// document.querySelector("#quote").textContent = `"${quotes[randomQuote]}"`
+let showBackdrop = true;
+
+const goals = ["Read a book.", "Do the laundry.", "Write a poem.", "Go for a walk.", "Workout at the gym."]
+const randomQuote = Math.floor(Math.random() * goals.length);
+document.querySelector("#goal").textContent = `🎯${goals[randomQuote]}`
 
 document.querySelector("#title").textContent = `Title: ${currentTitle}`;
 
@@ -63,30 +92,84 @@ fetch(`https://api.themoviedb.org/3/search/movie?api_key=64cbe67ca110f541ec519ba
                 lockedMovieIcon.textContent = "🔒"
                 lockedMovieBtn.textContent = "Unlock movie";
             }
+
             
-            fetch(`https://api.themoviedb.org/3/movie/${data.results[randomMovieNumber].id}/images?api_key=64cbe67ca110f541ec519ba56ec890b2`, { 
+            fetch(`https://api.themoviedb.org/3/movie/${data.results[randomMovieNumber].id}?api_key=64cbe67ca110f541ec519ba56ec890b2&query=${currentTitle}&include_adult=false&append_to_response=release_dates`, { 
+                method: "GET",
+                header: "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGNiZTY3Y2ExMTBmNTQxZWM1MTliYTU2ZWM4OTBiMiIsIm5iZiI6MTc4NjU1MzMwNS4xOTkwMDAxLCJzdWIiOiI2YTdjYTNkOTU4ZDU5MGZmZDBhZTY4MzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KaRDaQ0tzDzj5ZrcjBmj5TZ2uA8HER25NdcZpqeeApw"
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                console.log(data.release_dates.results)
+                const release_data = [];
+                let certification = "";
+                for (let release_date of data.release_dates.results) {
+                    if (release_date.iso_3166_1 === "US") {
+                        release_data.push(release_date);
+                    }
+                }
+                if ((release_data[0].release_dates[0].certification === "PG-13" && allowPG13RatedContent === "false") 
+                    || (release_data[0].release_dates[0].certification === "R" && allowMatureRatedContent === "false") 
+                    || (release_data[0].release_dates[0].certification === "NC-17" && allowMatureRatedContent === "false")) {
+                    certification = release_data[0].release_dates[0].certification;
+                    showBackdrop = false;
+                }
+
+                const genres = [];
+                const nsfwGenres = [];
+                for (let genre of data.genres) {
+                    genres.push(genre.name);
+                }
+                for (let genreName of genres) {
+                    if ((genreName === "Horror" && allowHorrorGenreContent === "false") 
+                        || (genreName === "Romance" && allowRomanceGenreContent === "false") 
+                        || (genreName === "Thriller" && allowThrillerGenreContent === "false")) {
+                        nsfwGenres.push(`${genreName}`);
+                        nsfwGenres.length = 1;
+                        showBackdrop = false;
+                     }
+                }
+
+                let contentWarning = ""
+                if (nsfwGenres.length !== 0 && certification !== "") {
+                    contentWarning = nsfwGenres + ", " + certification;
+                } else if (nsfwGenres.length !== 0 && certification === "") {
+                    contentWarning = nsfwGenres;
+                } else if (nsfwGenres.length === 0 && certification !== "") {
+                    contentWarning = certification;
+                }
+
+                if (showBackdrop === false) {
+                    document.querySelector("#title").textContent = `Title: ${currentTitle} ||`;
+                    document.querySelector("#nsfw-warning").textContent = `Content Warning: ${contentWarning} (Disabled)`;
+                }
+
+                if (storedDisplayTaglineCheckedValue !== "false") {
+                    if (data.tagline !== null && data.tagline !== "") {
+                        document.querySelector("#tagline").textContent = `"${data.tagline}"`;
+                    }
+                }
+            })
+
+        document.querySelector("#poster-container").innerHTML = `
+            <img id="poster" src="https://image.tmdb.org/t/p/original/${data.results[randomMovieNumber].poster_path}" title="${data.results[randomMovieNumber].title} (${data.results[randomMovieNumber].release_date.slice(0, 4)})">
+        `
+
+        fetch(`https://api.themoviedb.org/3/movie/${data.results[randomMovieNumber].id}/images?api_key=64cbe67ca110f541ec519ba56ec890b2`, { 
                 method: "GET",
                 header: "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGNiZTY3Y2ExMTBmNTQxZWM1MTliYTU2ZWM4OTBiMiIsIm5iZiI6MTc4NjU1MzMwNS4xOTkwMDAxLCJzdWIiOiI2YTdjYTNkOTU4ZDU5MGZmZDBhZTY4MzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KaRDaQ0tzDzj5ZrcjBmj5TZ2uA8HER25NdcZpqeeApw"
             })
                 .then(res => res.json())
                 .then(data => {
                     console.log(data.backdrops)
-                    let randomBackdropNumber = Math.floor(Math.random() * 100);
-                    if (data.backdrops.length < 100) {
-                        randomBackdropNumber = Math.floor(Math.random() * data.backdrops.length)
-                    }
-                    if (storedBackdropNumber === null) {
-                        document.body.style.background = `url("https://image.tmdb.org/t/p/original/${data.backdrops[randomBackdropNumber].file_path}") no-repeat center center fixed`
-                        document.body.style.backgroundSize = `cover`;
-                    } else if (storedBackdropNumber !== null) {
+                    let randomBackdropNumber = Math.floor(Math.random() * data.backdrops.length);
+                    if (storedBackdropNumber !== null) {
                         randomBackdropNumber = storedBackdropNumber;
                         lockedBackdropIcon.textContent = "🔒"
                         lockedBackdropBtn.textContent = "Unlock backdrop";
-                        document.body.style.background = `url("https://image.tmdb.org/t/p/original/${data.backdrops[randomBackdropNumber].file_path}") no-repeat center center fixed`
-                        document.body.style.backgroundSize = `cover`;
                     }
 
-                    console.log(randomBackdropNumber);
                     lockedBackdropBtn.addEventListener("click", (e) => {
                         e.preventDefault();
                         if (lockedBackdropIcon.textContent === "🔓") {
@@ -105,58 +188,60 @@ fetch(`https://api.themoviedb.org/3/search/movie?api_key=64cbe67ca110f541ec519ba
                         }
                     })
 
-                    document.querySelector("#current-backdrop").textContent = `Backdrop #${randomBackdropNumber} of ${data.backdrops.length}`
+                    document.querySelector("#change-backdrop-form").addEventListener("submit", (e) => {
+                        e.preventDefault();
+                        let searchedBackdropNumber = document.querySelector("#backdrop-number").value
+                        if (searchedBackdropNumber == "00" || searchedBackdropNumber == "000" || searchedBackdropNumber >= data.backdrops.length) {
+                            searchedBackdropNumber = 0;
+                        }
+                        localStorage.setItem("locked-backdrop-number", searchedBackdropNumber);
+                        if (storedMovieNumber === null) {
+                            localStorage.setItem("locked-movie-number", randomMovieNumber);
+                        }
+                        window.location.reload();
+                    })
+
+                    if (showBackdrop === true) {
+                        document.body.style.background = `url("https://image.tmdb.org/t/p/original/${data.backdrops[randomBackdropNumber].file_path}") no-repeat center center fixed`
+                        document.body.style.backgroundSize = `cover`;
+                        document.querySelector("#change-backdrop-form").style.display = "flex";
+                        document.querySelector("#current-backdrop-overview").style.display = "flex";
+                    } else {
+
+                    }
+
+                    document.querySelector("#current-backdrop").textContent = `Backdrop #${randomBackdropNumber} of ${data.backdrops.length - 1}`
                 });
 
-            document.querySelector("#poster-container").innerHTML = `
-                <img id="poster" src="https://image.tmdb.org/t/p/original/${data.results[randomMovieNumber].poster_path}" title="${data.results[randomMovieNumber].title} (${data.results[randomMovieNumber].release_date.slice(0, 4)})">
-            `
-            
-            if (storedDisplayTaglineCheckedValue !== null && storedDisplayTaglineCheckedValue !== false) {
-                fetch(`https://api.themoviedb.org/3/movie/${data.results[randomMovieNumber].id}?api_key=64cbe67ca110f541ec519ba56ec890b2&query=${currentTitle}&include_adult=false`, { 
-                method: "GET",
-                header: "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGNiZTY3Y2ExMTBmNTQxZWM1MTliYTU2ZWM4OTBiMiIsIm5iZiI6MTc4NjU1MzMwNS4xOTkwMDAxLCJzdWIiOiI2YTdjYTNkOTU4ZDU5MGZmZDBhZTY4MzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KaRDaQ0tzDzj5ZrcjBmj5TZ2uA8HER25NdcZpqeeApw"
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    if (data.tagline !== null && data.tagline !== "") {
-                        document.querySelector("#tagline").textContent = `"${data.tagline}"`;
-                    }
-                })
+        lockedMovieBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            if (lockedMovieIcon.textContent === "🔓") {
+                lockedMovieIcon.textContent = "🔒"
+                lockedMovieBtn.textContent = "Unlock movie";
+                localStorage.setItem("locked-movie-number", randomMovieNumber);
+            } else {
+                lockedMovieIcon.textContent = "🔓"
+                lockedMovieBtn.textContent = "Lock movie";
+                localStorage.removeItem("locked-movie-number");
+                if (lockedBackdropIcon.textContent === "🔒") {
+                    lockedBackdropIcon.textContent = "🔓"
+                    lockedBackdropBtn.textContent = "Lock backdrop";
+                    localStorage.removeItem("locked-backdrop-number");
+                }
             }
-            document.querySelector("#change-backdrop-form").addEventListener("submit", (e) => {
-                e.preventDefault();
-                let searchedBackdropNumber = document.querySelector("#backdrop-number").value
-                localStorage.setItem("locked-backdrop-number", searchedBackdropNumber);
-                if (storedMovieNumber === null) {
-                    localStorage.setItem("locked-movie-number", randomMovieNumber);
-                }
-                window.location.reload();
-            })
+        })
+    } else {
+        document.querySelector("#poster-container").innerHTML = `
+            <p id="poster" class="no-movie" title="This Movie Doesn't Exist. (${currentYear})">This Movie Doesn't Exist.</p>
+        `
 
-            lockedMovieBtn.addEventListener("click", (e) => {
-                e.preventDefault();
-                if (lockedMovieIcon.textContent === "🔓") {
-                    lockedMovieIcon.textContent = "🔒"
-                    lockedMovieBtn.textContent = "Unlock movie";
-                    localStorage.setItem("locked-movie-number", randomMovieNumber);
-                } else {
-                    lockedMovieIcon.textContent = "🔓"
-                    lockedMovieBtn.textContent = "Lock movie";
-                    localStorage.removeItem("locked-movie-number");
-                }
-            })
-        } else {
-            document.querySelector("#poster-container").innerHTML = `
-                <p id="poster" class="no-movie" title="This Movie Doesn't Exist. (${currentYear})">This Movie Doesn't Exist.</p>
-            `
-
-            document.body.style.background = "black"
-            document.body.style.backgroundSize = `cover`;
-            document.querySelector("#title").textContent = `Title: ${currentTitle} (This Movie Doesn't Exist.)`;
-        }
-    });
+        document.body.style.background = "black"
+        document.body.style.backgroundSize = `cover`;
+        document.querySelector("#title").textContent = `Title: ${currentTitle} (This Movie Doesn't Exist.)`;
+        document.querySelector("#change-backdrop-form").style.display = "none";
+        document.querySelector("#current-backdrop-overview").style.display = "none";
+    }
+});
 
 document.querySelector("#search-bar-form").addEventListener("submit", (e) => {
     e.preventDefault();
--- settings.js
+++ settings.js
@@ -1,62 +1,69 @@
-let currentTextColor = "#FFFFFF";
-let currentOutlineColor = "#000000";
-const storedTextColor = localStorage.getItem("text-color")
-if (storedTextColor !== null) {
-    currentTextColor = storedTextColor;
-}
-const storedOutlineColor = localStorage.getItem("outline-color")
-if (storedOutlineColor !== null) {
-    currentOutlineColor = storedOutlineColor;
-}
+// color
+const storedTextColor = localStorage.getItem("text-color");
+const storedOutlineColor = localStorage.getItem("outline-color");
+// tagline
 const storedDisplayTaglineCheckedValue = localStorage.getItem("display-tagline")
-if (storedDisplayTaglineCheckedValue !== null && storedDisplayTaglineCheckedValue !== false) {
+if (storedDisplayTaglineCheckedValue !== "false") {
     document.querySelector("#display-tagline").checked = true;
 }
+// nsfw
+const storedNsfwContent1CheckedValue = localStorage.getItem("allow-mature-rated-content")
+if (storedNsfwContent1CheckedValue !== "false") {
+    document.querySelector("#allow-mature-rated-content").checked = true;
+}
+const storedNsfwContent2CheckedValue = localStorage.getItem("allow-pg-13-rated-content")
+if (storedNsfwContent2CheckedValue !== "false") {
+    document.querySelector("#allow-pg-13-rated-content").checked = true;
+}
+const storedNsfwContent3CheckedValue = localStorage.getItem("allow-horror-genre-content")
+if (storedNsfwContent3CheckedValue !== "false") {
+    document.querySelector("#allow-horror-genre-content").checked = true;
+}
+const storedNsfwContent4CheckedValue = localStorage.getItem("allow-romance-genre-content")
+if (storedNsfwContent4CheckedValue !== "false") {
+    document.querySelector("#allow-romance-genre-content").checked = true;
+}
+const storedNsfwContent5CheckedValue = localStorage.getItem("allow-thriller-genre-content")
+if (storedNsfwContent5CheckedValue !== "false") {
+    document.querySelector("#allow-thriller-genre-content").checked = true;
+}
 
-document.querySelector("#text-color").value = currentTextColor;
-document.querySelector("#preview").style.color = currentTextColor;
-document.querySelector("#outline-color").value = currentOutlineColor;
-document.querySelector("#preview").style.textShadow = `1px 1px 0 ${currentOutlineColor}`;
-
-document.querySelector("#allow-horror").checked = "checked";
+document.querySelector("#text-color").value = storedTextColor;
+document.querySelector("#preview").style.color = storedTextColor;
+document.querySelector("#outline-color").value = storedOutlineColor;
+document.querySelector("#preview").style.textShadow = `1px 1px 0 ${storedOutlineColor}`;
 
 document.querySelector("#preview").textContent = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
 
 document.querySelector("#settings-form").addEventListener("submit", (e) => {
     e.preventDefault();
     alert("Your changes have been saved.")
+    // colors
     let selectedTextColor = document.querySelector("#text-color").value;
     localStorage.setItem("text-color", selectedTextColor);
     let selectedOutlineColor = document.querySelector("#outline-color").value;
     localStorage.setItem("outline-color", selectedOutlineColor);
+    // tagline
     let displayTagline = document.querySelector("#display-tagline").checked;
-    if (displayTagline === true) {
-        localStorage.setItem("display-tagline", displayTagline);
-    } else if (storedDisplayTaglineCheckedValue !== null) {
-        localStorage.removeItem("display-tagline");
-    }
+    localStorage.setItem("display-tagline", displayTagline);
+    // nsfw
+    let nsfwContent1 = document.querySelector("#allow-mature-rated-content").checked;
+    localStorage.setItem("allow-mature-rated-content", nsfwContent1);
+    let nsfwContent2 = document.querySelector("#allow-pg-13-rated-content").checked;
+    localStorage.setItem("allow-pg-13-rated-content", nsfwContent2);
+    let nsfwContent3 = document.querySelector("#allow-horror-genre-content").checked;
+    localStorage.setItem("allow-horror-genre-content", nsfwContent3);
+    let nsfwContent4 = document.querySelector("#allow-romance-genre-content").checked;
+    localStorage.setItem("allow-romance-genre-content", nsfwContent4);
+    let nsfwContent5 = document.querySelector("#allow-thriller-genre-content").checked;
+    localStorage.setItem("allow-thriller-genre-content", nsfwContent5);
 })
 
 document.querySelector("#text-color").addEventListener("input", () => {
     let selectedTextColor = document.querySelector("#text-color").value;
     document.querySelector("#preview").style.color = selectedTextColor;
 })
-
 document.querySelector("#outline-color").addEventListener("input", () => {
     let selectedOutlineColor = document.querySelector("#outline-color").value;
     document.querySelector("#preview").style.textShadow = `1px 1px 0 ${selectedOutlineColor}`;
 })
-
-// fetch(`https://api.themoviedb.org/3/search/movie?api_key=64cbe67ca110f541ec519ba56ec890b2&query=Lord Of The Rings&include_adult=false`, { 
-//     method: "GET",
-//     header: "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGNiZTY3Y2ExMTBmNTQxZWM1MTliYTU2ZWM4OTBiMiIsIm5iZiI6MTc4NjU1MzMwNS4xOTkwMDAxLCJzdWIiOiI2YTdjYTNkOTU4ZDU5MGZmZDBhZTY4MzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KaRDaQ0tzDzj5ZrcjBmj5TZ2uA8HER25NdcZpqeeApw"
-// })
-//     .then(res => res.json())
-//     .then(data => console.log(data));
-
-// fetch(`https://api.themoviedb.org/3/movie/122/images?api_key=64cbe67ca110f541ec519ba56ec890b2`, { 
-//     method: "GET",
-//     header: "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGNiZTY3Y2ExMTBmNTQxZWM1MTliYTU2ZWM4OTBiMiIsIm5iZiI6MTc4NjU1MzMwNS4xOTkwMDAxLCJzdWIiOiI2YTdjYTNkOTU4ZDU5MGZmZDBhZTY4MzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KaRDaQ0tzDzj5ZrcjBmj5TZ2uA8HER25NdcZpqeeApw"
-// })
-//     .then(res => res.json())
-//     .then(data => console.log(data));
